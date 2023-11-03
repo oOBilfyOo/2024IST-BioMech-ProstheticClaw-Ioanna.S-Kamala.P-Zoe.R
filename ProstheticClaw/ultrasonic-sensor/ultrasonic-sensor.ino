3d26cc86e59668faaabdfe4879ada9598f27f112
foodtech3 <126630787+foodtech3@users.noreply.github.com>
2023-11-03 11:49:54 +1100

    Update ultrasonic-sensor.ino

--- ProstheticClaw/ultrasonic-sensor/ultrasonic-sensor.ino
+++ ProstheticClaw/ultrasonic-sensor/ultrasonic-sensor.ino
@@ -1,14 +1,14 @@
-
 /*
-  Purpose: Basic example of the Seead Ultrasonic Ranger module
-  Notes: Connect to a digital PIN
+  Purpose: Basic example of the momentary button switch input module
+  Notes: 
+    1. Connect to a Digital pin
+    2. Normally open momentary switch
+    3. Wired for pull up only 
   Author: Ioanna, Kamala, Zoe
   Contact: Ioanna.sotiropoulou@education.nsw.gov.au, Kamala.pradhan@education.nsw.gov.au, Zoe.rath@education.nsw.gov.au 
-  Source: https://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/
-  Library Source: https://github.com/Seeed-Studio/Seeed_Arduino_UltrasonicRanger
-*/ 
-  
-*/ 
+ Source: https://wiki.seeedstudio.com/Grove-Button/
+*/
+
 // This is a different Ultrasonic Library to the one we used in the bootcamp becuase we are using a Seead grove sensor not a generic 4 pin Ultrasonic sensor
 #include "Ultrasonic.h"
 
