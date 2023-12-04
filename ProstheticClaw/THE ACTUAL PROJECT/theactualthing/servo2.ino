4e9310200b8ef5e56abd2d269f6aefe74a031520
✨Zoë✨ <126206627+ProcrastiNova@users.noreply.github.com>
2023-12-05 10:32:39 +1100

    added stuff to the main code (FINALLY)

--- ProstheticClaw/THE ACTUAL PROJECT/theactualthing/servo2.ino
+++ ProstheticClaw/THE ACTUAL PROJECT/theactualthing/servo2.ino
@@ -1,22 +1,4 @@
-/*
-  Author: 
-  Learning Intention: The students will learn how to connect and control a servo motor.
-  Success Criteria:
-    1. I understand how to connect the servo motor
-    2. I can manually write different degrees of movement to the servo
-    3. I can map a potentiometer to a servo and control its movement
-    4. I understand that a 180deg servo angle of movement is set by a frequency signal sent from the microcontroller
 
-  Student Notes: 
-
-  Documentation:
-    https://www.sparkfun.com/servos
-    https://github.com/arduino-libraries/Servo <-- We are still using this library
-
-  Schematic:
-    https://www.tinkercad.com/things/lQ9RyYJRoLn?sharecode=MKlN0A7R0WGodkdTRKkPJO7I8PeI5L_GCR7pCclQ0qM
-    https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/10.servoMotor/Bootcamp-servoMotor.png
-*/
 
 #include <Servo.h> 
 #include "Ultrasonic.h"
@@ -28,7 +10,7 @@ static unsigned int myUSPin = 6;
 Ultrasonic myUSsensor(myUSPin);
 bool isGateOpen;
 
-void setup() {
+void ServoMotorPartTwo() {
 myServo.attach (myServoPin);
 myServo.write(0);
 isGateOpen = false;
--- ProstheticClaw/THE ACTUAL PROJECT/theactualthing/theactualthing.ino
+++ ProstheticClaw/THE ACTUAL PROJECT/theactualthing/theactualthing.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+ button();
+ setupUSSENSOR ();
+ LineFinder()
+ ServoNumberone();
+ ServoMotorPartTwo();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+ PushButton()
 }
